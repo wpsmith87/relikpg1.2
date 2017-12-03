dcfef1225628d78def1fe6629a1a06e930448acf
wpsmith87 <wpsmith87@gmail.com>
2017-12-02 22:30:55 -0600

    commit message

--- js/slide.js
+++ js/slide.js
@@ -1,13 +1,40 @@
+// Modal script
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
 
 
-$("#slideshow > div:gt(0)").hide();
+function showModal(){
 
-setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-}, 3000);
+    modal.style.display = "block";
 
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function myFunction() {
+    myVar = setTimeout(showModal, 5000);
+}
+
+$(document).ready(function() {
+    myFunction();  
+});
