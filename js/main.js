15108f6eebe77d0fcf068cb17bee033013ed5525
wpsmith87 <wpsmith87@gmail.com>
2018-02-16 22:39:39 -0600

    some tweaks

--- js/main.js
+++ js/main.js
@@ -5,15 +5,25 @@ function showRow () {
         $(document).find(".projbutt").hide();
         $(document).find(".projbutts").show();
     });
+
+    $(".projbutta").click(function () { 
+        $(document).find(".proja").show();
+        $(document).find(".projbutta").hide();
+        $(document).find(".projbuttas").show();
+    });
 }
 
 function hideRow () {
     $(".projbutts").click(function () { 
+    	$(document).find(".projs").hide();
+    	$(document).find(".projbutts").hide();
+    	$(document).find(".projbutt").show();
+    });
 
-$(document).find(".projs").hide();
-$(document).find(".projbutts").hide();
-$(document).find(".projbutt").show();
-
+    $(".projbuttas").click(function () { 
+    	$(document).find(".proja").hide();
+    	$(document).find(".projbuttas").hide();
+    	$(document).find(".projbutta").show();
     });
 }
 
@@ -21,5 +31,7 @@ $(document).ready(function(){
 showRow();
 hideRow();
 $(this).find(".projs").hide(); 
+$(this).find(".proja").hide(); 
 $(this).find(".projbutts").hide();
+$(this).find(".projbuttas").hide();
 });
